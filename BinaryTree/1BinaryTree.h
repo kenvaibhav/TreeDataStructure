8aa0f990a5d29f18deb0cb8fdfa22bea1b366ad2
Vaibhav Kendre <106550562+kenvaibhav@users.noreply.github.com>
2023-08-16 22:40:57 +0530

    Add files via upload

---
+++ BinaryTree/1BinaryTree.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<typename T>
+class BinaryTreeNode 
+{
+    public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode()
+    {
+        delete left;
+        delete right;
+    }
+};
