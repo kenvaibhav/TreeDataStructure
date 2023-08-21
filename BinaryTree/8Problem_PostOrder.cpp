724524063626c7171cb4c310d2dd5f913277effd
Vaibhav Kendre <106550562+kenvaibhav@users.noreply.github.com>
2023-08-21 23:07:57 +0530

    Add files via upload

---
+++ BinaryTree/8Problem_PostOrder.cpp
@@ -0,0 +1,90 @@
+/*
+Postorder Binary Tree
+Send Feedback
+For a given Binary Tree of integers, print the post-order traversal.
+ALTIMAGE
+
+The postorder traversal for the given binary tree will be 40 50 20 30 10
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The only line of output prints the post-order traversal of the given binary tree.
+Constraints:
+1 <= N <= 10^6
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+ Sample Output 1:
+4 5 2 6 7 3 1 
+Sample Input 2:
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+ Sample Output 1:
+2 9 3 6 10 5 
+*/
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+void postOrder(BinaryTreeNode<int> *root) {
+	// Write your code here
+	if(root == NULL){
+		return;
+	}else{
+		postOrder(root -> left);
+		postOrder(root -> right);
+	}
+	cout<<root->data<<" ";
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    postOrder(root);
+}
